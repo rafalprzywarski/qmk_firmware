6bb769c83e8acb8b1d7a8356969d60b69ef22d3b
Rafal Przywarski <rafal.przywarski@gmail.com>
2021-10-03 18:03:06 +0200

    Mac GH60 generating proper Apple-specific keys (F3, F4)

--- keyboards/mac_gh60/config.h
+++ keyboards/mac_gh60/config.h
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID    0xFEED
-#define PRODUCT_ID   0x0000
+#define VENDOR_ID    0x05AC
+#define PRODUCT_ID   0x024F
 #define DEVICE_VER   0x0001
 #define MANUFACTURER Rafal Przywarski
 #define PRODUCT      Mac GH60
